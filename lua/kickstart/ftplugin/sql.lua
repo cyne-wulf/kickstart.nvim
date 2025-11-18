7deb54a7367ebfd5a58b3725b830f6cdcb5f93af
Ashan Devine <54266829+cyne-wulf@users.noreply.github.com>
2025-11-18 10:51:48 -0800

    Made postgresql first class

--- lua/kickstart/ftplugin/sql.lua
+++ lua/kickstart/ftplugin/sql.lua
@@ -6,3 +6,9 @@ vim.opt_local.commentstring = '-- %s'
 
 -- 🔑 enable dadbod-completion backend
 vim.bo.omnifunc = 'vim_dadbod_completion#omni'
+
+-- Default to postgres dialect
+if not vim.b.sql_type or vim.b.sql_type == '' then
+  vim.b.sql_type = 'pgsql' -- Used by your custom Blink dadbod adapter
+  vim.b.sql_type_override = 'pgsql' -- Used by Vim's built-in syntax highlighting
+end
